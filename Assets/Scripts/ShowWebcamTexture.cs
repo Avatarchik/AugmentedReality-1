4d72ebdb2754b1a7a2ebcd2f23d5999300ffea43
Maumov <mau.movilla@gmail.com>
2016-05-13 18:49:30 -0500

    started

---
+++ Assets/Scripts/ShowWebcamTexture.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowWebcamTexture : MonoBehaviour {
+
+	// Use this for initialization
+	void Start() {
+		Debug.Log (WebCamTexture.devices.Length);
+		foreach (WebCamDevice wd in WebCamTexture.devices) {
+			Debug.Log (wd.name);
+		}
+
+		WebCamTexture webcamTexture = new WebCamTexture();
+		webcamTexture.requestedFPS = 60;
+		Renderer renderer = GetComponent<Renderer>();
+		renderer.material.mainTexture = webcamTexture;
+		webcamTexture.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
